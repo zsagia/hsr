9f9cab3445ea13680c11e0e308c2f4e593ae19f5
Markus Mohoritsch <markus.mohoritsch@jipp.it>
2016-09-07 03:21:07 +0200

    - fixed routes config

--- src/app/config/routes.config.ts
+++ src/app/config/routes.config.ts
@@ -1,6 +1,5 @@
 import { Test1Component } from '../components/test1/test1.component';
 import { Test11Component } from '../components/test11/test11.component';
-import { Test2Component } from '../components/test2/test2.component';
 import { RegisterComponent } from '../components/register/register.component';
 import { LoginComponent } from '../components/login/login.component';
 import { FirebaseAuthService } from '../shared/firebase-auth.service';
@@ -16,7 +15,6 @@ export const ROUTES_CONFIG = [
   {path: 'login', component: LoginComponent},
   {path: 'test1', component: Test1Component, canActivate: [FirebaseAuthService]},
   {path: 'test11/:id', component: Test11Component, canActivate: [FirebaseAuthService]},
-  {path: 'test2', component: Test2Component, canActivate: [FirebaseAuthService]},
   {path: 'blog', component: BlogComponent, text: 'Blog', canActivate: [FirebaseAuthService]},
   {path: 'fotos', component: FotosComponent, text: 'Fotos', canActivate: [FirebaseAuthService]},
   {path: 'flyer', component: FlyerComponent, text: 'Flyer', canActivate: [FirebaseAuthService]},
