a11a93bcc3afdd4c9126cb55aaf462df87e70319
motabass <markus.mohoritsch@gmx.at>
2017-03-13 21:35:18 +0100

    updated angular-cli

--- karma.conf.js
+++ karma.conf.js
@@ -8,7 +8,7 @@ module.exports = function (config) {
     plugins: [
       require('karma-jasmine'),
       require('karma-chrome-launcher'),
-      require('karma-remap-istanbul'),
+      require('karma-coverage-istanbul-reporter'),
       require('angular-cli/plugins/karma')
     ],
     files: [
@@ -17,18 +17,16 @@ module.exports = function (config) {
     preprocessors: {
       './src/test.ts': ['angular-cli']
     },
-    remapIstanbulReporter: {
-      reports: {
-        html: 'coverage',
-        lcovonly: './coverage/coverage.lcov'
-      }
+    coverageIstanbulReporter: {
+      reports: [ 'html', 'lcovonly' ],
+      fixWebpackSourcePaths: true
     },
     angularCli: {
-      config: './angular-cli.json',
+      config: './.angular-cli.json',
       environment: 'dev'
     },
     reporters: config.angularCli && config.angularCli.codeCoverage
-      ? ['progress', 'karma-remap-istanbul']
+      ? ['progress', 'coverage-istanbul']
       : ['progress'],
     port: 9876,
     colors: true,
