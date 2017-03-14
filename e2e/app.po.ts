dd98b18ee670b13975a916903d0aadfe0a34554b
motabass <markus.mohoritsch@gmx.at>
2017-03-14 21:03:04 +0100

    angular update umbau

--- e2e/app.po.ts
+++ e2e/app.po.ts
@@ -1,6 +1,6 @@
-import { browser, element, by } from 'protractor';
+import { browser, by, element } from 'protractor';
 
-export class HsrPage {
+export class CheckPage {
   navigateTo() {
     return browser.get('/');
   }
