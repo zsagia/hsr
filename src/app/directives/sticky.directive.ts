e4dc529574f117becaa5d87696577518cc7598f8
motabass <markus.mohoritsch@gmx.at>
2017-03-14 23:44:03 +0100

    sticky toolbar

--- src/app/directives/sticky.directive.ts
+++ src/app/directives/sticky.directive.ts
@@ -7,7 +7,7 @@ export class StickyDirective {
 
   constructor(element: ElementRef) {
     window.addEventListener('scroll', (e) => {
-      if (window.pageYOffset > 64) {
+      if (window.pageYOffset > 1) {
         element.nativeElement.classList.add('sticky');
       } else {
         element.nativeElement.classList.remove('sticky');
