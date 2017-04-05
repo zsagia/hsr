d0a6ae30ac3f3351c87a431a8174a205683b333d
motabass <markus.mohoritsch@gmx.at>
2017-04-05 22:20:49 +0200

    e2e tests

--- e2e/app.e2e-spec.ts
+++ e2e/app.e2e-spec.ts
@@ -1,14 +1,35 @@
-import { CheckPage } from './app.po';
+import { browser, by, element } from 'protractor';
+import { PageFrame } from './app.po';
 
 describe('check App', () => {
-  let page: CheckPage;
+  let page: PageFrame;
 
   beforeEach(() => {
-    page = new CheckPage();
+    // browser.waitForAngularEnabled(false);
+    page = new PageFrame();
   });
 
   it('should display message saying app works', () => {
     page.navigateTo();
-    expect(page.getParagraphText()).toEqual('app works!');
+    expect(page.loginButton.getText()).toEqual('Login');
+
+     // expect(page.isLogoutButtonDisplayed()).toBe(false);
+    page.loginButton.click();
+
+    page.emailField.sendKeys('markus.mohoritsch@gmx.at');
+    page.passwordField.sendKeys('0meinemail.');
+    page.submitButton.click();
+
+    expect(page.logoutButton).toBeDefined();
+    expect(element(by.binding('notPresent')).isPresent()).toBe(false);
   });
+
+  // afterEach(() => {
+  //   browser.manage().logs().get('browser').then(browserLog => {
+  //     // expect(browserLog.length).toEqual(0);
+  //     if (browserLog.length) {
+  //       console.error('log: ' + JSON.stringify(browserLog));
+  //     }
+  //   });
+  // });
 });
--- protractor.conf.js
+++ protractor.conf.js
@@ -2,18 +2,21 @@
 // https://github.com/angular/protractor/blob/master/lib/config.ts
 
 const {SpecReporter} = require('jasmine-spec-reporter');
+const HtmlReporter = require('protractor-html-screenshot-reporter');
 
 exports.config = {
   allScriptsTimeout: 11000,
   specs: [
     './e2e/**/*.e2e-spec.ts'
   ],
+  // seleniumAddress: 'http://localhost:9515',
   capabilities: {
     'browserName': 'chrome'
   },
   directConnect: true,
   baseUrl: 'http://localhost:4200/',
-  framework: 'jasmine',
+  framework: 'jasmine2',
+  useAllAngular2AppRoots: true,
   jasmineNodeOpts: {
     showColors: true,
     defaultTimeoutInterval: 30000,
@@ -26,5 +29,6 @@ exports.config = {
   },
   onPrepare() {
     jasmine.getEnv().addReporter(new SpecReporter({spec: {displayStacktrace: true}}));
+    jasmine.getEnv().addReporter(new HtmlReporter({baseDirectory: '/tmp/screenshots'}));
   }
 };
