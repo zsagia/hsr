8e0673eba4a26d3aa7c8829c54a7d57fc7cf8501
motabass <0meinemail>
2017-07-06 19:45:02 +0200

    fixed

--- src/app/modules/fotos/config/gallery.config.ts
+++ src/app/modules/fotos/config/gallery.config.ts
@@ -1,27 +1,38 @@
 import { GalleryConfig } from 'ng-gallery';
 
 export const GALLERY_CONFIG: GalleryConfig = {
-  'style': {
-    'background': '#000000',
-    'width': '1000px',
-    'height': '600px'
+  style: {
+    background: '#121519',
+    width: '900px',
+    height: '500px'
   },
-  'animation': 'fade',
-  'loader': {
-    'width': '50px',
-    'height': '50px',
-    'position': 'center',
-    'icon': 'oval'
+  animation: 'fade',
+  loader: {
+    width: '50px',
+    height: '50px',
+    position: 'center',
+    icon: 'oval'
   },
-  'description': {
-    'position': 'bottom',
-    'overlay': true,
-    'text': true,
-    'counter': true
+  description: {
+    position: 'bottom',
+    overlay: false,
+    text: true,
+    counter: true
   },
-  'bullets': false,
-  'player': false,
-  'thumbnails': false,
-  'navigation': true,
-  'gestures': true
+  bullets: {
+    position: 'bottom'
+  },
+  navigation: {
+    position: 'top'
+  },
+  player: {
+    autoplay: false,
+    speed: 3000
+  },
+  thumbnails: {
+    width: 120,
+    height: 90,
+    position: 'top',
+    space: 20
+  }
 };
